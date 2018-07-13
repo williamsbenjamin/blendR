f79b6c5ad38a908dcbfe79b7d4a7442360b93042
williamsbenjamin <benjamin@smu.edu>
2018-07-13 17:35:49 -0500

    Add in sampled data (with self-report info) and self-reports, along with descriptions

---
+++ R/self_reports_data.R
@@ -0,0 +1,12 @@
+## Self-Reports
+
+#' @title Self-Reports
+#'
+#'@format A dataframe with 117 rows and 2 variables:
+#'
+#'\describe{
+#' \item{anglers}{Number of anglers reported by captain for fishing trip}
+#' \item{number_kept}{Number of fish kept, as reported by captains for a fishing trip}
+#'}
+
+"self_reports"
